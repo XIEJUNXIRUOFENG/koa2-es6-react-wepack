add04f1928bbb330fa6627029f05929d9f8a8958
xiejun <547699825@qq.com>
2018-01-08 20:09:16 +0800

    导航图标名称修改

--- src/containers/index.js
+++ src/containers/index.js
@@ -34,81 +34,78 @@ class App extends Component {
 	}
 
 	onCollapse = (collapsed) => {
-			this.setState({ collapsed });
+		this.setState({ collapsed });
 	}
 
 	changeMode = (key) => {
-			this.setState({changed:key.key})
+		this.setState({changed:key.key})
 	}
 
 	toggleCollapsed = () => {
-			this.setState({
-				collapsed: !this.state.collapsed,
-			});
+		this.setState({
+			collapsed: !this.state.collapsed,
+		});
 	}
 	
 	render() {
-		// console.warn('需要', this.props)
-			return (
-					<Layout style={{ minHeight: '100vh' }}>
-							<Sider
-							collapsible
-							collapsed={this.state.collapsed}
-							onCollapse={this.onCollapse}
-							>
-									<div>
-											<Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
-													<Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
-											</Button>
-											<Menu
-													defaultSelectedKeys={['1']}
-													defaultOpenKeys={['sub1']}
-													mode="inline"
-													theme="dark"
-													inlineCollapsed={this.state.collapsed}
-													onClick={this.changeMode}
-											>
-													<Menu.Item key="1">
-															<Icon type="pie-chart" />
-															<span>Option 1</span>
-													</Menu.Item>
-													<Menu.Item key="2">
-															<Icon type="desktop" />
-															<span>Option 2</span>
-													</Menu.Item>
-													<Menu.Item key="3">
-															<Icon type="inbox" />
-															<span>Option 3</span>
-													</Menu.Item>
-													<SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-															<Menu.Item key="5">Option 5</Menu.Item>
-															<Menu.Item key="6">Option 6</Menu.Item>
-															<Menu.Item key="7">Option 7</Menu.Item>
-															<Menu.Item key="8">Option 8</Menu.Item>
-													</SubMenu>
-													<SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
-															<Menu.Item key="9">Option 9</Menu.Item>
-															<Menu.Item key="10">Option 10</Menu.Item>
-															<SubMenu key="sub3" title="Submenu">
-																	<Menu.Item key="11">Option 11</Menu.Item>
-																	<Menu.Item key="12">Option 12</Menu.Item>
-															</SubMenu>
-													</SubMenu>
-											</Menu>
-									</div>
-							</Sider>
-							<Layout>
-									{/* <Header style={{ background: '#fff', padding: 0 }} /> */}
-									<Content  id='main' style={{ margin: '0 16px' }}>
-											{ContentDict[this.state.changed]}
-											{/* <WeatherTable />*/}
-									</Content>
-									<Footer style={{ textAlign: 'center' }}>
-											Writed by XIE-ZHANG
-									</Footer>
-							</Layout>                   
-					</Layout>
-			);
+		return (
+			<Layout style={{ minHeight: '100vh' }}>
+				<Sider
+				collapsible
+				collapsed={this.state.collapsed}
+				onCollapse={this.onCollapse}
+				>
+					<div>
+						<Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
+								<Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
+						</Button>
+						<Menu
+							defaultSelectedKeys={['1']}
+							defaultOpenKeys={['sub1']}
+							mode="inline"
+							theme="dark"
+							inlineCollapsed={this.state.collapsed}
+							onClick={this.changeMode}
+						>
+							<Menu.Item key="1">
+								<Icon type="desktop" />
+								<span>重要城市天气预报</span>
+							</Menu.Item>
+							<Menu.Item key="2">
+								<Icon type="table" />
+								<span>表格天气预报</span>
+							</Menu.Item>
+							<Menu.Item key="3">
+								<Icon type="line-chart" />
+								<span>图形天气预报</span>
+							</Menu.Item>
+							<SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
+								<Menu.Item key="5">Option 5</Menu.Item>
+								<Menu.Item key="6">Option 6</Menu.Item>
+								<Menu.Item key="7">Option 7</Menu.Item>
+								<Menu.Item key="8">Option 8</Menu.Item>
+							</SubMenu>
+							<SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
+								<Menu.Item key="9">Option 9</Menu.Item>
+								<Menu.Item key="10">Option 10</Menu.Item>
+								<SubMenu key="sub3" title="Submenu">
+									<Menu.Item key="11">Option 11</Menu.Item>
+									<Menu.Item key="12">Option 12</Menu.Item>
+								</SubMenu>
+							</SubMenu>
+						</Menu>
+					</div>
+				</Sider>
+				<Layout>
+					<Content  id='main' style={{ margin: '0 16px' }}>
+							{ContentDict[this.state.changed]}
+					</Content>
+					<Footer style={{ textAlign: 'center' }}>
+							Writed by XIE-ZHANG
+					</Footer>
+				</Layout>                   
+			</Layout>
+		);
 	}
 }
 
