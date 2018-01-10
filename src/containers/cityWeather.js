193b386e380c28f37267f419aa71abc87965a811
David Zhang <641206446@qq.com>
2018-01-10 17:46:08 +0800

    Weather carousel map

--- src/containers/cityWeather.js
+++ src/containers/cityWeather.js
@@ -1,27 +1,27 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { Button } from 'antd';
+import { Button } from 'antd'
 import { selectWeather } from '../action/action'
 import { SELECT_WEATHER } from '../action/action'
+import '../theme/cityWeather.less'
 
 class CityWeather extends Component {
 	constructor(props) {
 		super(props)
   }
   
-	getWeather = () => {
-		const { dispatch } = this.props
-    dispatch(selectWeather('city=320100&key=ecfbe66140f8ed4cc0bcbcf92b111074'))
-	}
-
 	componentWillReceiveProps(nextProps) {
   }
 
 	render() {
-		const { weatherDetail } = this.props
 		return (
-			<div>
-				<Button type="primary" onClick={this.getWeather}>点击获取天气</Button>
+			<div className="city-weather">
+				<div className="city-container">
+					<div className="img img01"></div>
+					<div className="img img02"></div>
+					<div className="img img03"></div>
+					<div className="img img04"></div>
+				</div>
 			</div>
 		)
 	}
